594f0a888e223160bd1fecfc90b9553d8fa32642
SturmTim <Sturm.Tim@sus.htl-grieskirchen.at>
2020-09-22 17:23:48 +0200

    Beispiel 1 Fertig

--- src/com/SturmTim/EratosthenesPrimeSieve.java
+++ src/com/SturmTim/EratosthenesPrimeSieve.java
@@ -13,40 +13,51 @@ import java.util.Scanner;
  *
  * @author tsturm18
  */
-public class EratosthenesPrimeSieve implements PrimeSieve{
+public class EratosthenesPrimeSieve implements PrimeSieve {
 
-    
+    List<Integer> isprime = new ArrayList<>();
+    List<Integer> notprime = new ArrayList<>();
+
     public static void main(String[] args) {
-        
+        EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve();
+        eps.printPrimes();
     }
 
     @Override
     public boolean isPrime(int p) {
-        return true;
+        if (notprime.contains(p)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
     public void printPrimes() {
-        
-        
-    }
-    
-    public void getPrimeArray(){
-        Scanner scanner =new Scanner(System.in, "Windows-1252");
+        Scanner scanner = new Scanner(System.in, "Windows-1252");
         System.out.println("Bis zu welcher Obergrenze wollen Sie die Primzahlen wissen?");
         int og = Integer.parseInt(scanner.nextLine());
-        
-        List<Integer> isprime = new ArrayList<>();
-        List<Integer> notprime = new ArrayList<>();
-        
+
         for (int i = 2; i <= og; i++) {
-            if(notprime.contains(i)){
-            }
-            else{
+            if (isPrime(i) == false) {
+                notprime.add(i);
+            } else {
                 isprime.add(i);
+                for (int j = i * i; j <= og; j = j + i) {
+                    notprime.add(j);
+                }
             }
-            
         }
+        int zeilenumbruch = 5;
+        for (Integer integer : isprime) {
+            if (zeilenumbruch == 0) {
+                zeilenumbruch = 5;
+                System.out.println("");
+            }
+            System.out.print(integer + ", ");
+            zeilenumbruch = zeilenumbruch - 1;
+        }
+
     }
-    
+
 }
